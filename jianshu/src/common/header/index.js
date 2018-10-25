5fd5396d180f7016001f0dedb478a6b416d4f4be
kai <kai@163.com>
2018-10-26 01:51:42 +0800

    CSSTransition动画

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import {
     HeaderWrapper,
     Logo,
@@ -11,7 +12,16 @@ import {
 
 } from './style';
 
+
 class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            focused: false
+        }
+        this.handleInputFocus = this.handleInputFocus.bind(this);
+        this.handleInputBlur = this.handleInputBlur.bind(this);
+    }
     render() {
         return (
             <HeaderWrapper>
@@ -24,20 +34,41 @@ class Header extends Component {
                         <i className="iconfont">&#xe636;</i>
                     </NavItem>
                     <SearchWrapper>
-                        <NavSearch></NavSearch>
-                        <i className="iconfont">&#xe614;</i>
-                    </SearchWrapper>
-                    </Nav>
-                    <Addition>
-                        <Button className='writting'>
-                            <i className="iconfont">&#xe615;</i>
-                            写文章</Button>
-                        <Button className='reg'>注册</Button>
+                        <CSSTransition
+                            in= {this.state.focused}
+                            timeout={200}
+                            classNames="slide"
+                        >
+                            <NavSearch
+                                className={this.state.focused ? 'focused' : ''}
+                                onFocus={this.handleInputFocus}
+                                onBlur={this.handleInputBlur}
+                            ></NavSearch>
+                        </CSSTransition>
+                        <i className={this.state.focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
 
-                    </Addition>
+                    </SearchWrapper>
+                </Nav>
+                <Addition>
+                    <Button className='writting'>
+                        <i className="iconfont">&#xe615;</i>
+                        写文章</Button>
+                    <Button className='reg'>注册</Button>
+
+                </Addition>
             </HeaderWrapper>
         )
 
+    }
+    handleInputFocus() {
+        this.setState(
+            { focused: true }
+        )
+    }
+    handleInputBlur() {
+        this.setState(
+            { focused: false }
+        )
     }
 }
 
--- jianshu/src/common/header/style.js
+++ jianshu/src/common/header/style.js
@@ -55,13 +55,17 @@ export const SearchWrapper = styled.div`
         line-height:30px;
         border-radius:15px;
         text-align:center;
+        &.focused{
+            background: #777;
+            color: #fff;
+        }
     }
 `;
 
-export const NavSearch = styled.input.attrs({placeholder: '搜索'})`
+export const NavSearch = styled.input.attrs({ placeholder: '搜索' })`
     width: 160px;
     height: 38px;
-    padding: 0 20px;
+    padding: 0 30px 0 20px;
     margin-top: 9px;
     margin-left: 20px;
     box-sizing: border-box;
@@ -69,9 +73,27 @@ export const NavSearch = styled.input.attrs({placeholder: '搜索'})`
     outline: none;
     border-radius: 19px;
     background: #eee;
-    front-size:14px;
+    font-size:14px;
+    color: #666;
     &::placeholder{
         color: #999;
+    }
+    &.focused{
+        width: 240px ;
+    }
+
+    &.slide-enter{
+        width:160px;
+        transition:all .2s ease-out;
+    }
+    &.slide-enter-active{
+        width:240px;
+    }
+    &.slide-exit{
+        transition:all .2s ease-out;
+    }
+    &.slide-exit-active{
+        width:160px;
     }
 `;
 
