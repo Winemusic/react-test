7c3f14e95f4e61f0be170df8a4e3afe17a0d6b2f
kai <kai@163.com>
2018-11-05 00:43:20 +0800

    7-12 Ajax

--- jianshu/src/common/header/store/actionCreators.js
+++ jianshu/src/common/header/store/actionCreators.js
@@ -1,9 +1,28 @@
 import * as constants from './constants';
+import axios from 'axios';
+import { fromJS } from 'immutable';
 
-export const searchFocus = ()=>({
+export const searchFocus = () => ({
     type: constants.SEARCH_FOCUS
 });
 
-export const searchBlur = ()=>({
+export const searchBlur = () => ({
     type: constants.SEARCH_BLUR
+});
+
+const changeList = (data) => ({
+    type: constants.CHANGE_LIST,
+    data:fromJS(data)
 })
+
+export const getList = () => {
+    return (dispatch) => {
+        axios.get('/api/headerList.json').then((res) => {
+            const data = res.data;
+            dispatch(changeList(data.data));
+        })
+            .catch(() => {
+                console.log('error');
+            })
+    }
+}
--- jianshu/src/common/header/store/reducer.js
+++ jianshu/src/common/header/store/reducer.js
@@ -1,17 +1,24 @@
 import * as constants from './constants';
-import {fromJS} from 'immutable';
+import { fromJS } from 'immutable';
 
 const defaultState = fromJS({
-    focused: false
+    focused: false,
+    list: []
 });
 
-export default (state = defaultState,action)=>{
-    if(action.type === constants.SEARCH_FOCUS){
-        return state.set('focused', true );
+export default (state = defaultState, action) => {
+    if (action.type === constants.SEARCH_FOCUS) {
+        return state.set('focused', true);
     }
 
-    if(action.type === constants.SEARCH_BLUR){
-        return state.set('focused', false );
+    if (action.type === constants.SEARCH_BLUR) {
+        return state.set('focused', false);
     }
+
+    if (action.type === constants.CHANGE_LIST) {
+        return state.set('list', action.data);
+        // console.log(action);
+    }
+
     return state;
 }
